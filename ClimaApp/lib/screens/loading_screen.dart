cf6e876854260fa1f4349195352b070e0432adb6
Alexici <104847485+Alexici@users.noreply.github.com>
2023-06-05 14:36:06 +0200

    commit-1

--- ClimaApp/lib/screens/loading_screen.dart
+++ ClimaApp/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,12 +9,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      print("Requested for Permission");
+      if (permission == LocationPermission.deniedForever) {
+        Future.error('Permission Denied Forever');
+      }
+    }
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      print('Location Permission Granted Always or While In Use');
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            getLocation();
+          },
           child: const Text('Get Location'),
         ),
       ),
