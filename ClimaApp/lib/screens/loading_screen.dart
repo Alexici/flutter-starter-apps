883f8c4e6521020acc0394115552e9319896fe70
Alexici <104847485+Alexici@users.noreply.github.com>
2023-06-10 20:53:16 +0200

    another commit

--- ClimaApp/lib/screens/loading_screen.dart
+++ ClimaApp/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:clima_app/key.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -12,6 +15,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+
+    double latitude = location.latitude;
+    double longitude = location.longitude;
+
+    getData(latitude, longitude);
+  }
+
+  void getData(double lat, double long) async {
+    var url = Uri.https('api.weatherapi.com', '/v1/current.json',
+        {'key': apiKey, 'q': '$lat,$long'});
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double tempDouble = decodedData['current']['temp_c'];
+      int temp = tempDouble.toInt();
+      String condition = decodedData['current']['condition']['text'];
+      String cityName = decodedData['location']['name'];
+
+      print("$temp,$condition,$cityName");
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
